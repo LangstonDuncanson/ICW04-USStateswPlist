e5abc4222fbc2f4690a3f7316c569e14fa998c25
microgoz <andres.gonzalez2392@gmail.com>
2017-11-06 18:59:15 -0500

    Initial Commit

---
+++ sc00-PlistAndTableViews/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sc00-PlistAndTableViews
+//
+//  Created by Andres Gonzalez on 11/6/17.
+//  Copyright © 2017 Andres Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sc00-PlistAndTableViews/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  sc00-PlistAndTableViews
+//
+//  Created by Andres Gonzalez on 11/6/17.
+//  Copyright © 2017 Andres Gonzalez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
